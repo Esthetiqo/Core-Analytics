f457bf90e4951a1f3f9fdcca26da777e8caa8cf2
lemonpac <lemonpac@intentx.io>
2024-02-17 01:17:55 +0100

    Using base environment

--- dashboard/src/environments/environment.ts
+++ dashboard/src/environments/environment.ts
@@ -1,6 +1,9 @@
-import {EnvironmentInterface} from "./environment-interface"
+import { EnvironmentInterface } from "./environment-interface";
+import { environment as base82Env } from "./environment.base_8_2";
 
 export const environment: EnvironmentInterface = {
-    name: "",
-    assetsFolder: "",
-}
+  name: "Core",
+  assetsFolder: "core",
+  singleAffiliateAccountSource: "0x1c03B6480a4efC2d4123ba90d7857f0e1878B780",
+  environments: [base82Env],
+};
